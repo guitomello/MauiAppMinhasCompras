1588ddaa0ec5bbf82379488cc3f4a4e6e30fff3d
guitomello <melloguito@gmail.com>
2026-08-16 16:52:25 -0300

    Code Update

--- MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper(string path) 
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync <Produto> ().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+
+        }
+            
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produtos SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql,
+                p.Descricao,
+                p.Quantidade,
+                p.Preco,
+                p.Id
+            );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll(int id)
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string query)
+        {
+            string sql = "SELECT * WHERE Descricao LIKE '%" + query + "%'";
+
+            return _conn.QueryAsync<Produto>(
+                sql
+            );
+        }
+
     }
 }
